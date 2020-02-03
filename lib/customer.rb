aa7a153e197ddd3f3e484cc87181df6c6a27f9bf
Tororoi <cantwell.tom@gmail.com>
2020-02-03 14:48:34 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,31 @@
+require_relative 'meal'
+
 class Customer
   
+    attr_reader :name
+    attr_accessor :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.customer == self}
+    end
+
+    def waiters
+        meals.map(&:waiter)
+    end
 end
